6c23468315c037ba2163a2c156bad90b0c3c9c04
nishantbinary <72297516+nishantbinary@users.noreply.github.com>
2020-10-03 13:59:54 +0530

    Update temp.py
    All of it in PyQt5

--- temp.py
+++ temp.py
@@ -1,5 +1,12 @@
+import sys
+import matplotlib
 import json
-import matplotlib.pyplot as plt
+matplotlib.use('Qt5Agg')
+
+from PyQt5 import QtCore, QtWidgets
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
 
 file = open("temp.txt","r")
 data = file.readlines()
@@ -14,13 +21,30 @@ for i in range(len(data)):
 for i in range(1,3044):
 	humx.append(i)
 	tempx.append(i)
-	
-plt.plot(humx, humy)
-plt.title('Humidity & Temperature(5AM - 5PM)')
-plt.xlabel('Time')
-plt.ylabel('Humidity, Temperature')
 
-plt.plot(tempx, tempy)
-plt.xlabel('Time')
-plt.legend(['Humidity', 'Temperature'], loc=4)
-plt.show()
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        # Create the maptlotlib FigureCanvas object, 
+        # which defines a single set of axes as self.axes.
+        sc = MplCanvas(self, width=10, height=10, dpi=100)
+        sc.axes.plot(humx , humy)
+        sc.axes.plot(tempx , tempy)
+        self.setCentralWidget(sc)
+
+        self.show()
+
+
+app = QtWidgets.QApplication(sys.argv)
+w = MainWindow()
+app.exec_()
